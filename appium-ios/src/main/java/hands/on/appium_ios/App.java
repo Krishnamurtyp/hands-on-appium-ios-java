692302519c77373101661938c6489f2a98e54303
Gaurav Khanna <gaurav.khanna@tiladmins-MacBook-Pro-22.local>
2020-02-05 13:15:26 +0530

    new examples

---
+++ appium-ios/src/main/java/hands/on/appium_ios/App.java
@@ -0,0 +1,13 @@
+package hands.on.appium_ios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
