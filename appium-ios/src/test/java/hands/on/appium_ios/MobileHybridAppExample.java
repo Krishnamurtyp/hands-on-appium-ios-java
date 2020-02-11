2576664ab50d910c0862a44450e9ac368ba89576
Gaurav Khanna <gaurav.khanna@tiladmins-MacBook-Pro-22.local>
2020-02-11 21:17:33 +0530

    new fixes

--- appium-ios/src/test/java/hands/on/appium_ios/MobileHybridAppExample.java
+++ appium-ios/src/test/java/hands/on/appium_ios/MobileHybridAppExample.java
@@ -1,60 +1,13 @@
 package hands.on.appium_ios;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
-
-import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.ios.IOSElement;
-import io.appium.java_client.remote.IOSMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
 
 public class MobileHybridAppExample {
 
 	@Test
 	public void test001() throws MalformedURLException {
-
-		IOSDriver<IOSElement> iosDriver;
-
-		URL url;
-
-		DesiredCapabilities desiredCapabilities;
-
-		url = new URL("http://127.0.0.1:4723/wd/hub");
-
-		desiredCapabilities = new DesiredCapabilities();
-
-		desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.3");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.UDID, "3B73633F-25BE-4CAF-A231-F6FAE97E17FE");
-
-		// desiredCapabilities.setCapability(MobileCapabilityType.APP,"/Users/gaurav.khanna/Downloads/Calculator_1.0.ipa");
-
-		// desiredCapabilities.setCapability(IOSMobileCapabilityType.BUNDLE_ID,
-		// "com.apple.reminders");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "safari");
-
-		iosDriver = new IOSDriver<IOSElement>(url, desiredCapabilities);
-
-		// iosDriver = new IOSDriver<IOSElement>(desiredCapabilities);
-
-		// iosDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		iosDriver.get("https://not-just-a-tester.blogspot.com/");
-
-		String title = iosDriver.getTitle();
-
-		System.out.println("Page Title : " + title);
-
-		iosDriver.quit();
 
 	}
 
--- appium-ios/src/test/java/hands/on/appium_ios/MobileNativeAppExample.java
+++ appium-ios/src/test/java/hands/on/appium_ios/MobileNativeAppExample.java
@@ -1,61 +1,70 @@
 package hands.on.appium_ios;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.Test;
-
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
+
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MobileNativeAppExample {
 
 	@Test
-	public void test001() throws MalformedURLException {
+	public void testAppActivity() throws Exception {
 
-		IOSDriver<IOSElement> iosDriver;
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("platformName", "iOS");
+		desiredCapabilities.setCapability("platformVersion", "13.3");
+		desiredCapabilities.setCapability("deviceName", "iPhone 8");
+		desiredCapabilities.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "io.cloudgrey.the-app");
 
-		URL url;
+		IOSDriver<WebElement> iosDriver = new IOSDriver<WebElement>(new URL("http://localhost:4723/wd/hub"),
+				desiredCapabilities);
 
-		DesiredCapabilities desiredCapabilities;
-
-		url = new URL("http://127.0.0.1:4723/wd/hub");
-
-		desiredCapabilities = new DesiredCapabilities();
-
-		desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.3");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.UDID, "3B73633F-25BE-4CAF-A231-F6FAE97E17FE");
-
-		// desiredCapabilities.setCapability(MobileCapabilityType.APP,"/Users/gaurav.khanna/Downloads/Calculator_1.0.ipa");
-
-		// desiredCapabilities.setCapability(IOSMobileCapabilityType.BUNDLE_ID,
-		// "com.apple.reminders");
-
-		desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "safari");
-
-		iosDriver = new IOSDriver<IOSElement>(url, desiredCapabilities);
-
-		// iosDriver = new IOSDriver<IOSElement>(desiredCapabilities);
-
-		// iosDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		iosDriver.get("https://not-just-a-tester.blogspot.com/");
-
-		String title = iosDriver.getTitle();
-
-		System.out.println("Page Title : " + title);
+		performActions(iosDriver);
 
 		iosDriver.quit();
 
 	}
 
+	public void performActions(IOSDriver<WebElement> iosDriverObj) {
+
+		System.out.println(
+				iosDriverObj.findElement(MobileBy.AccessibilityId("Choose An Awesome View")).getAttribute("name"));
+
+		System.out.println(
+				iosDriverObj.findElement(MobileBy.AccessibilityId("Choose An Awesome View")).getAttribute("label"));
+
+		System.out.println(
+				iosDriverObj.findElement(MobileBy.AccessibilityId("Choose An Awesome View")).getAttribute("enabled"));
+
+		System.out.println(
+				iosDriverObj.findElement(MobileBy.AccessibilityId("Choose An Awesome View")).getAttribute("visible"));
+
+		System.out.println(
+				iosDriverObj.findElement(MobileBy.AccessibilityId("Choose An Awesome View")).getAttribute("label"));
+
+		WebDriverWait wait = new WebDriverWait(iosDriverObj, 10);
+
+		wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Echo Box"))).click();
+
+		wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//XCUIElementTypeTextField[@name=\"messageInput\"]")))
+				.sendKeys("Hello World");
+
+		wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("messageSaveBtn"))).click();
+
+		String savedText = wait
+				.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("savedMessage"))).getText();
+
+		Assert.assertEquals(savedText, "Hello World");
+
+		iosDriverObj.navigate().back();
+	}
 }
